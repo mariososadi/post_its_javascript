afd4af3ddb266f51be25d8426bfc2105828b62c5
mariososadi <mariososadi@gmail.com>
2016-08-05 17:55:15 -0500

    Add files via upload

---
+++ post-it.js
@@ -0,0 +1,59 @@
+var Board = function( selector ) {
+  // Aqui denerá ir el código que tenga que ver con tu tablero 
+  
+  // Utiliza esta sintaxis para referirte al selector que representa al tablero.
+  // De esta manera no dependerás tanto de tu HTML.  
+  var $elem = $( selector );
+  
+  function initialize() {
+    $elem.on('dblclick', function(e){
+
+      var x = e.pageX + 'px';
+      var y = e.pageY + 'px';
+      var div = $('<div id="master" class="post-it"><div class="header"><div class="close"><strong>X</strong></div></div><div class="content">...</div>').css({                 
+            "left": x,
+            "top": y
+        });
+      $(this).append(div);   
+      PostIt();
+    });
+  };
+
+  initialize();
+};
+
+var PostIt = function() {
+
+      var $x = $( "strong" )  
+      $x.on('click', function(e){
+       e.preventDefault();
+       $(this).parents('.post-it').remove();
+      });
+
+      $( ".post-it" ).draggable({
+        stack: ".post-it"
+      });
+      $(".post-it").dblclick( function(e) {
+        return false
+      });
+
+     var $h = $( ".header" )  
+       $h.on('click', function(event){
+        $(this).prop( "contenteditable", true );
+        event.stopPropagation();
+     });
+
+
+    
+  
+
+
+  // Aquí va el código relacionado con un post-it
+};
+
+$(function() {
+  // Esta es la fucnión que correrá cuando este listo el DOM 
+  new Board('#board');
+
+  });
+
